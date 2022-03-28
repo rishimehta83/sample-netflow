9dded8a876b87e64adab536a4a0e0f117d275c9a
Rishi Mehta <rmehta@rmehta-a02.vmware.com>
2022-03-28 11:19:35 -0700

    Working Solution

--- README.md
+++ README.md
@@ -10,19 +10,13 @@ docker exec -it netflowdb /bin/sh
 
 psql -U postgres
 
-'[{"src_app": "foo", "dest_app": "bar", "vpc_id": "vpc-0", "bytes_tx":
-100, "bytes_rx": 500, "hour": 1}]'
-
-
 CREATE TABLE netflow (
-  id SERIAL,
   src_app    VARCHAR(250) NOT NULL,
   dest_app   VARCHAR(250) NOT NULL,
   vpc_id     VARCHAR(250) NOT NULL,
   bytes_tx   NUMERIC,
   bytes_rx   NUMERIC,
   hour       NUMERIC,
-  PRIMARY KEY (src_app, dest_app, vpc_id)
 );
 
 
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
         "strconv"
-//        "io/ioutil"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,17 +11,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-
-type Person struct {
-	SrcApp     string `json:"src_app"`
-	DestApp string `json:"dest_app"`
-}
-
 type NetFlow struct {
-	SrcApp     string `json:"src_app"`
-}
-
-type NetFlow2 struct {
 	SrcApp     string `json:"src_app"`
 	DestApp    string `json:"dest_app"`
 	VpcID      string `json:"vpc_id"`
@@ -41,7 +30,6 @@ const (
 )
 
 func OpenConnection() *sql.DB {
-        fmt.Println("Connection Established\n")
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -61,25 +49,22 @@ func OpenConnection() *sql.DB {
 
 func GETHandler(w http.ResponseWriter, r *http.Request) {
 	db := OpenConnection()
-        fmt.Printf("Rhea Mehta GET\n")
 
         p, err := strconv.Atoi(r.URL.Query().Get("hour"))       
-        
-        userSql := "SELECT * FROM netflow WHERE hour = $1" 
+        userSql := "select distinct src_app, dest_app, vpc_id, sum(bytes_tx) as bytes_tx, sum(bytes_rx) as bytes_rx, hour from netflow where hour = $1 group by (src_app, dest_app, vpc_id, hour);"
         rows, err := db.Query(userSql, p)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var flows []NetFlow2
+	var flows []NetFlow
 
 	for rows.Next() {
-		var flow NetFlow2
+		var flow NetFlow
 		err := rows.Scan(&flow.SrcApp, &flow.DestApp, &flow.VpcID, &flow.BytesTx, &flow.BytesRx, &flow.Hour)
                 if err != nil {
 		   log.Fatal(err)
                 }
-                fmt.Printf("%+v\n", flow)
 		flows = append(flows, flow)
 	}
 
@@ -94,34 +79,37 @@ func GETHandler(w http.ResponseWriter, r *http.Request) {
 
 func POSTHandler(w http.ResponseWriter, r *http.Request) {
 	db := OpenConnection()
-        fmt.Printf("Rhea Mehta POST\n")
 
-	var p []NetFlow2
-        fmt.Printf("Rhea Mehta inserting in the table\n")
+	var p []NetFlow
 	err := json.NewDecoder(r.Body).Decode(&p)
-        fmt.Printf("Rhea Mehta inserting in the table\n")
 	if err != nil {
-                fmt.Printf("Rhea Mehta inserting in the table error\n")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	sqlStatement := `INSERT INTO netflow (src_app, dest_app, vpc_id, bytes_tx, bytes_rx, hour) VALUES ($1, $2, $3, $4, $5, $6)`
        	_, err2 := db.Exec(sqlStatement, p[0].SrcApp, p[0].DestApp, p[0].VpcID, p[0].BytesTx, p[0].BytesRx, p[0].Hour)
-        fmt.Printf("Rhea Mehta inserting in the table\n")
 	if err2 != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		panic(err)
 	}
 
-        fmt.Printf("Rhea Mehta inserting in the table done\n")
 	w.WriteHeader(http.StatusOK)
 	defer db.Close()
 }
 
+
+func ReqHandler(w http.ResponseWriter, r *http.Request) {
+   switch r.Method {
+      case "GET":
+          GETHandler(w, r)
+      case "POST":
+          POSTHandler(w, r)
+
+   }
+}
+
 func main() {
-        fmt.Println("Received Request\n")
-	http.HandleFunc("/flows", GETHandler)
-	http.HandleFunc("/insert", POSTHandler)
+	http.HandleFunc("/flows", ReqHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
